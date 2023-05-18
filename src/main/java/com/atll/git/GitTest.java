80c9632fcf41176c1f9d0091087941fb8e6730e1
ggboom <ikun@126.com>
2023-05-18 23:23:41 +0800

    first commit

---
+++ src/main/java/com/atll/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atll.git;
+
+/**
+ * @auther ggboom
+ * @create 2023-05-18 23:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
